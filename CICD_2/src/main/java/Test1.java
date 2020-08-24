479709e5edcf2b9aa52e85c7bdc0ea13d967a3a2
nit29sin <nitish29singla@gmail.com>
2020-08-24 15:20:26 +0530

    Commit1

---
+++ CICD_2/src/main/java/Test1.java
@@ -0,0 +1,20 @@
+public class Test1 {
+	private int n=10;
+	public void f1(int n1)
+	{
+		n1=10;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n2=20;
+		System.out.println("Hello Sonar");
+		if (n2==10)
+		{
+			System.out.println("False Part");
+		}
+		
+
+	}
+
+}
